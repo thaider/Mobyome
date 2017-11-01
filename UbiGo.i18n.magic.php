1b9698d0989575615b297f565c1bd72eaed1569f
Tobias Haider <thai@kollabor.at>
2017-11-01 12:39:57 +0100

    Gemeinden, Statistik and more

--- UbiGo.i18n.magic.php
+++ UbiGo.i18n.magic.php
@@ -17,4 +17,9 @@ $magicWords['en'] = array(
 				'konto' => array( 0, 'konto' ),
 				'money' => array( 0, 'money' ),
 				'quellenlink' => array( 0, 'quellenlink' ),
+				'statistik' => array( 0, 'statistik' ),
+				'gemeinde' => array( 0, 'gemeinde' ),
+				'gemeindezahl' => array( 0, 'gemeindezahl' ),
+				'gkz' => array( 0, 'gkz' ),
+				'einzelticketpreissegment' => array( 0, 'einzelticketpreissegment' ),
 				);
